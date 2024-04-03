c58cbf57471377d005fb722d96e4ea29362d8fb3
Prakhar <prakhar0912@gmail.com>
2024-04-04 00:35:54 +0530

    arst

--- okay.sh
+++ okay.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 abap=$(sapcontrol -nr 00 -function GetSystemInstanceList | grep ABAP | grep GREEN)
 java=$(sapcontrol -nr 00 -function GetSystemInstanceList | grep J2EE | grep GREEN)
 
@@ -6,10 +5,15 @@ echo $host
 
 if [ -n abap ] && [ -n java ]; then
     echo $whoami ABAP and JAVA
+    output="ABAP and JAVA"
 elif [ -n abap ]; then
     echo $whoami ABAP
+    output="ABAP"
 elif [ -n java ]; then
     echo $whoami JAVA
+    output="JAVA"
 else
     echo ERROR
+    output="Error"
 fi
+
