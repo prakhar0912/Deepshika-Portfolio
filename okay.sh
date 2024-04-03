b546d70dcab542fecb4688bc11ddc80210e3b743
Prakhar <prakhar0912@gmail.com>
2024-04-04 00:20:22 +0530

    oku

--- okay.sh
+++ okay.sh
@@ -1,1 +1,15 @@
-ls -l
+#!/bin/bash
+abap=$(sapcontrol -nr 00 -function GetSystemInstanceList | grep ABAP | grep GREEN)
+java=$(sapcontrol -nr 00 -function GetSystemInstanceList | grep J2EE | grep GREEN)
+
+echo $host
+
+if [ -n abap ] && [ -n java ]; then
+    echo $whoami ABAP and JAVA
+elif [ -n abap ]; then
+    echo $whoami ABAP
+elif [ -n java ]; then
+    echo $whoami JAVA
+else
+    echo ERROR
+fi
