7eea45a4414a8767a9af821fdd74f190eb4c1866
Prakhar <prakhar0912@gmail.com>
2024-04-05 15:03:03 +0530

    arst

--- okay.sh
+++ okay.sh
@@ -21,13 +21,13 @@ fi
 declare -a stopServiceOutput=()
 for inst in "${instances[@]}"
 do
-    readarray -t stopServiceOutput < <(sapcontrol -nr ${inst} -function GetSystemInstanceList)
+    stopServiceOutput+=$(sapcontrol -nr ${inst} -function GetSystemInstanceList)
 done
 
 declare -a cleanIpcOutput=()
 for inst in "${instances[@]}"
 do
-    readarray -t stopServiceOutput < <(cleanipc ${inst} remove all)
+    cleanIpcOutput+=$(cleanipc ${inst} remove all)
 done
 
 declare -p stopServiceOutput
